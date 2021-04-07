b13baaefb46b743d78c99fc50d1851ae350e3bc5
arsendr <minezspeletajs@gmail.com>
2021-04-07 17:18:31 +0200

    commit of menu
    first commit of main menu with starting buttons

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Play loads scene of game, thus starting game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Quit game
+    public void QuitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
