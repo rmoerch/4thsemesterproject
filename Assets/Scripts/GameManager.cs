119b063393c0c0724c013c95c61e47d067664c99
arsendr <minezspeletajs@gmail.com>
2021-05-12 15:08:52 +0200

    game manager test
    test version of game manager, find it attached in main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Game States
+public enum GameState { NullStage, MainMenu, Game, Boss, GameOver }
+
+public delegate void OnStateChangeHandler();
+
+public class GameManager : MonoBehaviour
+{
+	protected GameManager() { }
+	public static GameManager instance = null;
+	public event OnStateChangeHandler OnStateChange;
+	public GameState gameState { get; private set; }
+
+    //public static GameManager Instance
+    //{
+    //	get
+    //	{
+    //		if (GameManager.instance == null)
+    //		{
+    //			GameManager.instance = new GameManager();
+    //			DontDestroyOnLoad(GameManager.instance);
+    //		}
+    //		return GameManager.instance;
+    //	}
+
+    //}
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetGameState(GameState state)
+	{
+		this.gameState = state;
+		OnStateChange();
+	}
+
+	public void OnApplicationQuit()
+	{
+		GameManager.instance = null;
+	}
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,16 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Play loads scene of game, thus starting game
+    GameManager gM;
+
+    //call instance of game manager
+    //set current stage to mainmenu
+    private void Awake()
+    {
+        gM = GameManager.instance;
+        gM.OnStateChange += HandleOnStateChange;
+        Debug.Log("Current game state when Awakes: " + gM.gameState);
+    }
+
+    //test current game state
+    private void Start()
+    {
+        Debug.Log("Current game state when Starts: " + gM.gameState);
+        gM.SetGameState(GameState.MainMenu);
+    }
+
+    //set the OnStateChange event, when changing scenes
+    public void HandleOnStateChange()
+    {
+        Debug.Log("Handling state change to: " + gM.gameState);
+    }
+
+
+
+    //Above new code for checking game states, doesnt seem to work atm
+    //-------------------------------------------
+    //Below is original main menu code
+    //These things dont conflict and dont break the game so i guess it is ok
+
+
+
+    //Load game scene, start the game
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //hardcoded load game method
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    //load main menu
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
