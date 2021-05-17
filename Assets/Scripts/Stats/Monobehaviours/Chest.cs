2e64b82e33ae95d4315ec77eb84966906aa78d5b
Bruno <vilicbruno@gmail.com>
2021-05-17 09:22:45 +0200

    Stuff that got stashed (chest)

---
+++ Assets/Scripts/Stats/Monobehaviours/Chest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Chest : MonoBehaviour
+{
+    public GameObject chestEffect;
+    public GameObject[] itemsToSpawn;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Hero"))
+        {
+            //StartCoroutine(SpawnAfterDelay());
+            Destroy(gameObject);
+            GameObject effect = Instantiate(chestEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1.5f);
+            //Invoke("ChestSpawn", 1.5f);
+            int randomIndex = Random.Range(0, itemsToSpawn.Length);
+            GameObject clone = Instantiate(itemsToSpawn[randomIndex], transform.position, Quaternion.identity);
+
+        }
+    }
+
+    //private void ChestSpawn()
+    //{
+    //    int randomIndex = Random.Range(0, itemsToSpawn.Length);
+    //    GameObject clone = Instantiate(itemsToSpawn[randomIndex], transform.position, Quaternion.identity);
+    //    Debug.Log("hello");
+    //}
+
+    //IEnumerator SpawnAfterDelay()
+    //{
+    //    yield return new WaitForSeconds(1.5f);
+    //    int randomIndex = Random.Range(0, itemsToSpawn.Length);
+    //    GameObject clone = Instantiate(itemsToSpawn[randomIndex], transform.position, Quaternion.identity);
+    //}
+}
