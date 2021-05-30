f275d6174ba66e3415b041cf3b4acdc0b54d365f
Rasmus <rasmusmoerch@hotmail.com>
2021-05-30 13:37:29 +0200

    Finished up story dialogue in intro room

--- Assets/Scripts/DialogueSystemScripts/DialogueManager.cs
+++ Assets/Scripts/DialogueSystemScripts/DialogueManager.cs
@@ -7,14 +7,16 @@ public class DialogueManager : MonoBehaviour
 {
     public Text nameText;
     public Text dialogueText;
-
+   
     public Animator animator;
-
-    private Queue<string> sentences;
+    private Queue<string> sentenceQueue;
+    private List<string> sentenceList;
 
     void Start()
     {
-        sentences = new Queue<string>();
+        sentenceQueue = new Queue<string>();
+        sentenceList = new List<string>();
+        
     }
 
     public void StartDialogue(Dialogue dialogue)
@@ -23,25 +25,53 @@ public class DialogueManager : MonoBehaviour
 
         nameText.text = dialogue.name;
 
-        sentences.Clear();
+        sentenceQueue.Clear();
 
         foreach (string sentence in dialogue.sentences)
         {
-            sentences.Enqueue(sentence);
+            sentenceQueue.Enqueue(sentence);
         }
 
-        DisplayNextSentece();
+        DisplayNextSentence();
+
+
+        //sentenceList.Clear();
+
+        //foreach (string sentence in dialogue.sentences)
+        //{
+        //    sentenceList.Add(sentence);
+        //}
+
+        //DisplayRandomSentence();
     }
 
-    public void DisplayNextSentece()
+    //public void DisplayRandomSentence()
+    //{
+
+    //    //If there are no sentences -> end the dialogue!
+    //    if(sentenceList.Count == 0)
+    //    {
+    //        EndDialogue();
+    //        return;
+    //    }
+
+    //    var sentence = Random.Range(0, sentenceList.Count);
+    //    dialogueText.text = sentenceList[sentence];
+    //    StopAllCoroutines();
+    //    StartCoroutine(TypeSentence(sentenceList[sentence]));
+
+    //}
+
+
+    public void DisplayNextSentence()
     {
-        if(sentences.Count == 0)
+        if (sentenceQueue.Count == 0)
         {
             EndDialogue();
             return;
         }
 
-        string sentence = sentences.Dequeue();
+        string sentence = sentenceQueue.Dequeue();
         dialogueText.text = sentence;
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
@@ -54,7 +84,7 @@ public class DialogueManager : MonoBehaviour
         foreach (char letter in sentence.ToCharArray())
         {
             dialogueText.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.03f);
         }
     }
 
@@ -63,5 +93,13 @@ public class DialogueManager : MonoBehaviour
     {
         animator.SetBool("IsOpen", false);
     }
-    
+
+    //public void EndDialogueByKeyPress()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        animator.SetBool("IsOpen", false);
+    //    }
+    //}
+
 }
