edef654fedbcd08e2a2bb48f1e6acec9f9622f4d
Bruno <vilicbruno@gmail.com>
2021-05-31 12:54:28 +0200

    1

--- Assets/WIP/Fuck/Spikes.cs
+++ Assets/WIP/Fuck/Spikes.cs
@@ -6,29 +6,9 @@ public class Spikes : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< Updated upstream:Assets/Scripts/CorutineManager.cs
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            instance = this;
-=======
-<<<<<<< Updated upstream:Assets/Scripts/Stats/Monobehaviours/CharacterInventory.cs
-        instance = this;
-=======
         if (collision.gameObject.CompareTag("Hero"))
         {
-            collision.gameObject.GetComponentInParent<PlayerHP>().LoseHP(10);
-        }
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            collision.gameObject.GetComponentInParent<EnemyHP>().LoseHP(10);
-
             collision.gameObject.GetComponentInParent<PlayerHP>().LoseHP(20);
->>>>>>> Stashed changes:Assets/WIP/Fuck/Spikes.cs
         }
->>>>>>> Stashed changes:Assets/WIP/Fuck/Spikes.cs
     }
 }
